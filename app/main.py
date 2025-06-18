09ef4f4888eeed123e42e2e2db8daf1bc24ae69c
Atakan AK <atakanak48@gmail.com>
2025-06-18 15:46:34 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     connection , _ = server.accept()
     received_message = connection.recv(1024)
     corr_id = int.from_bytes(received_message[8:12]).to_bytes(4,signed=True)
-    topic = int.from_bytes(received_message[35:40]).to_bytes(6,signed=True)
+    topic = int.from_bytes(received_message[36:40]).to_bytes(3,signed=True)
     print(f"Description: {topic}")
     message = (
         corr_id
