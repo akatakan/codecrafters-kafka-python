adfb8f1c230e9eca7ee5dadc69c6ceb5c6ec2ae7
Atakan AK <atakanak48@gmail.com>
2025-06-18 14:40:58 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -12,12 +12,16 @@ def main():
     api_version = int.from_bytes(received_message[6:8])
     corr_id = int.from_bytes(received_message[8:12]).to_bytes(4, signed=True)
     error_code = (0 if 0 <= api_version <= 4 else 35).to_bytes(2, signed=True)  # 35 is the error code for "Unsupported version"
-    num_of_apikeys = (len([api_key])+1 if api_key else 0).to_bytes(1, signed=False)
+    num_of_apikeys = (2).to_bytes(1, signed=False)
     message = (
         corr_id
         +error_code
         +num_of_apikeys
         +api_key.to_bytes(2, signed=True)
+        +(0).to_bytes(2, signed=True)
+        +(4).to_bytes(2, signed=True)
+        +(0).to_bytes(1, signed=False)
+        +(75).to_bytes(2, signed=True)
         +(0).to_bytes(2, signed=True)
         +(4).to_bytes(2, signed=True)
         +(0).to_bytes(1, signed=False)
