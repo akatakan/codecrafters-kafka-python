80c873ad23c45977e133050bd6e38621c1649170
Atakan AK <atakanak48@gmail.com>
2025-06-18 13:30:44 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,11 +10,10 @@ def main():
     received_message = connection.recv(1024)
     corr_id = int.from_bytes(received_message[8:12])
     api_version = int.from_bytes(received_message[6:8])
-    print(f"Ver: {api_version}")
+    print(f"Message: {int.from_bytes(received_message)}")
     error_code = 0 if 0 <= api_version < 4 else 35
-    print(f"Error code: {error_code}")
     message_size = len(received_message).to_bytes(4, signed=True)
-    connection.sendall(message_size+corr_id.to_bytes(4, signed=True)+(error_code).to_bytes(2,signed=True))
+    connection.sendall(message_size+corr_id.to_bytes(4, signed=True)+error_code.to_bytes(2,signed=True))
     
 if __name__ == "__main__":
     main()
