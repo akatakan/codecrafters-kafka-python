8cb096948cd29fca33124fa6285b14053ae77f1d
Atakan AK <atakanak48@gmail.com>
2025-06-18 13:41:13 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection,_ = server.accept() # wait for client
     received_message = connection.recv(1024)
-    api_key = int.from_bytes(received_message[4:6], signed=True)
-    api_version = int.from_bytes(received_message[6:8])
-    corr_id = int.from_bytes(received_message[8:12])
+    api_key = int.from_bytes(received_message[4:6], signed=True).to_bytes(2, signed=True)
+    api_version = int.from_bytes(received_message[6:8]).to_bytes(2, signed=True)
+    corr_id = int.from_bytes(received_message[8:12]).to_bytes(4, signed=True)
     print(f"Corr_id: {corr_id}, Api_key: {api_key}, Api_version: {api_version}")
-    error_code = 0 if 0 <= api_version < 4 else 35
+    error_code = (0 if 0 <= api_version < 4 else 35).to_bytes(2, signed=True)  # 35 is the error code for "Unsupported version"
     message_size = len(received_message).to_bytes(4, signed=True)
-    connection.sendall(message_size+corr_id.to_bytes(4, signed=True)+error_code.to_bytes(2,signed=True))
+    api_keys = len([api_key,0,4]).to_bytes(2, signed=True)  # 0 is the api key for "Produce" and 4 is the api key for "Fetch"
+    connection.sendall(message_size+corr_id+error_code+api_keys,api_key,(0).to_bytes(2, signed=True), (4).to_bytes(2, signed=True))
     
 if __name__ == "__main__":
     main()
