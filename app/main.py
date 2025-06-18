81cbc2a6b733a6c2bb9ac19ebf7f7253da60c25f
Atakan AK <atakanak48@gmail.com>
2025-06-18 12:30:44 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,12 +7,8 @@ def main():
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection,_ = server.accept() # wait for client
-    connection.recv(1024)
-    message_size = (35).to_bytes(4, signed=True)
-    request_api_key = (18).to_bytes(2, signed=True)
-    request_api_version = (4).to_bytes(2, signed=True)
-    correlation_id = (1870644833).to_bytes(4, signed=True).hex()
-    connection.sendall(message_size+request_api_key+request_api_version+correlation_id)  # send 0 to client
+    received_message = connection.recv(1024)
+    print(f"Received message: {received_message.decode('utf-8')}")
 
 
 if __name__ == "__main__":
