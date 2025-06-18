920570ad7e8579ba9cb987d286b42cf14da0733e
Atakan AK <atakanak48@gmail.com>
2025-06-18 11:56:14 +0300

    sending response with corr id

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    connection,_ = server.accept() # wait for client
+    connection.sendall((0).to_bytes(4, signed=True) + (7).to_bytes(4, signed=True))  # send 0 to client
 
 
 if __name__ == "__main__":
