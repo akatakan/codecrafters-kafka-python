51006f1a4ebac987190ec7fa3b124c646c2d1198
Atakan AK <atakanak48@gmail.com>
2025-06-18 15:35:25 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,34 +1,27 @@
 import socket  # noqa: F401
 import threading  # noqa: F401
-
-def send_message(connection):
-    connection,_ = connection
-    while True:
-        received_message = connection.recv(1024)
-        if not received_message:
-            break
-        corr_id = int.from_bytes(received_message[8:12]).to_bytes(4)
-        error_code = (3).to_bytes(2, signed=True)
-        desc = int.from_bytes(received_message[24:30]).to_bytes(4)
-        print(f"Description: {desc}")
-        message = (
-            corr_id
-            +error_code
-        )
-        
-        connection.sendall(
-            len(message).to_bytes(4, signed=False)
-            +message
-        )
+     
         
 def main():
 
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    while True:
-        connection = server.accept()
-        threading.Thread(target=send_message, args=(connection,),daemon=True).start()
+    connection , _ = server.accept()
+    received_message = connection.recv(1024)
+    corr_id = int.from_bytes(received_message[8:12]).to_bytes(4)
+    error_code = (3).to_bytes(2, signed=True)
+    desc = int.from_bytes(received_message[24:30]).to_bytes(4)
+    print(f"Description: {desc}")
+    message = (
+        corr_id
+        +error_code
+    )
+        
+    connection.sendall(
+        len(message).to_bytes(4, signed=False)
+        +message
+    )
 
     
     
