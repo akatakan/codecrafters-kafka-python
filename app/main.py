ab04d797580eb1ef47bf95eb377c5d39beda7621
Atakan AK <atakanak48@gmail.com>
2025-06-18 15:33:36 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,26 +7,14 @@ def send_message(connection):
         received_message = connection.recv(1024)
         if not received_message:
             break
-        api_key = int.from_bytes(received_message[4:6], signed=True)
-        api_version = int.from_bytes(received_message[6:8])
         corr_id = int.from_bytes(received_message[8:12]).to_bytes(4)
-        api_key_2 = int.from_bytes(received_message[12:14], signed=True)
-        error_code = (0 if 0 <= api_version <= 4 else 35).to_bytes(2, signed=True)
-        num_of_apikeys = (3).to_bytes(1, signed=False)
+        error_code = (3).to_bytes(2, signed=True)
+        desc = int.from_bytes(received_message[24:30]).to_bytes(4)
+        print(f"Description: {desc}")
         message = (
             corr_id
             +error_code
-            +num_of_apikeys
-            +api_key.to_bytes(2, signed=True)
-            +(0).to_bytes(2, signed=True)
-            +(4).to_bytes(2, signed=True)
-            +(0).to_bytes(1, signed=False)
-            +(75).to_bytes(2, signed=True)
-            +(0).to_bytes(2, signed=True)
-            +(0).to_bytes(2, signed=True)
-            +(0).to_bytes(1, signed=False)
-            +(0).to_bytes(4,signed=True)
-            +(0).to_bytes(1,signed=False))
+        )
         
         connection.sendall(
             len(message).to_bytes(4, signed=False)
