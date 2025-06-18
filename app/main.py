dd3099a806398432b254b303769c96dab99281ff
Atakan AK <atakanak48@gmail.com>
2025-06-18 12:31:31 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection,_ = server.accept() # wait for client
     received_message = connection.recv(1024)
-    print(f"Received message: {received_message.decode('utf-8')}")
+    print(f"Received message: {received_message}")
 
 
 if __name__ == "__main__":
