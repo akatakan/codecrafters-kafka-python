bde734218d7e615afee67fd6564c2da8f0044711
Atakan AK <atakanak48@gmail.com>
2025-06-18 13:37:29 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection,_ = server.accept() # wait for client
     received_message = connection.recv(1024)
-    corr_id = int.from_bytes(received_message[8:12])
+    api_key = int.from_bytes(received_message[4:6], signed=True)
     api_version = int.from_bytes(received_message[6:8])
-    print(f"Corr_id: {corr_id}")
+    corr_id = int.from_bytes(received_message[8:12])
+    print(f"Corr_id: {corr_id}, Api_key: {api_key}, Api_version: {api_version}")
     error_code = 0 if 0 <= api_version < 4 else 35
     message_size = len(received_message).to_bytes(4, signed=True)
     connection.sendall(message_size+corr_id.to_bytes(4, signed=True)+error_code.to_bytes(2,signed=True))
