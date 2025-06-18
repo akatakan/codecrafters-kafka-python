4f8179fee71b5dadc295569a25603f1d79c277ec
Atakan AK <atakanak48@gmail.com>
2025-06-18 16:17:12 +0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ def main():
     received_message = connection.recv(1024)
     corr_id = int.from_bytes(received_message[8:12]).to_bytes(4,signed=True)
     client_len = int.from_bytes(received_message[12:14], signed=True)
-    topic_len = int.from_bytes(received_message[14+client_len:14+client_len+1]).to_bytes(1,signed=True)
-    print("---->",topic_len)
-    topic_name = int.from_bytes(received_message[15+client_len+1:15+client_len+1+topic_len], signed=True).to_bytes(topic_len, signed=True)
+    topic__name_len = int.from_bytes(received_message[15+client_len:15+client_len+1]).to_bytes(1,signed=True)
+    print("---->",topic__name_len)
+    topic_name = int.from_bytes(received_message[15+client_len+1:topic__name_len], signed=True).to_bytes(topic__name_len, signed=True)
     message = (
         corr_id
         +(3).to_bytes(2, signed=True)
